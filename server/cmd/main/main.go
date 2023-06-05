e250194443bec2b788081358e2eb5b2dc9a608b4
amalvivek <avivek99@gmail.com>
2023-06-05 22:03:32 +0100

    move files

---
+++ server/cmd/main/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"snoop-server/pkg/controllers"
+	"snoop-server/pkg/database"
+)
+
+func main() {
+	ctx := context.Background()
+	defer database.UnsafeClose(ctx)
+
+	router := gin.Default()
+
+	router.Use(database.ErrorHandler)
+
+	// Landlord APIs
+	router.GET("/landlords", controllers.SearchLandlords)
+	router.GET("/landlords/:id", controllers.GetLandlord)
+	router.POST("/landlords", controllers.AddLandlord)
+
+	// Properties APIs
+	router.GET("/properties", controllers.SearchProperties)
+	router.GET("/properties/:id", controllers.GetProperty)
+	router.POST("/properties", controllers.AddProperty)
+
+	//router.GET("/albums", getAlbums)
+	//router.GET("/albums/:id", getAlbumByID)
+	//router.POST("/albums", postAlbums)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
+}
---
+++ server/pkg/controllers/landlords.go
@@ -0,0 +1,78 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"log"
+	"net/http"
+	"snoop-server/pkg/database"
+)
+
+type LandlordResult struct {
+	Landlord `json:"landlord"`
+}
+
+type Landlord struct {
+	ID      string `json:"id"`
+	Name    string `json:"name,omitempty"`
+	Email   string `json:"email,omitempty"`
+	Contact string `json:"contact,omitempty"`
+}
+
+func (l Landlord) Reconstruct(record *neo4j.Record) Landlord {
+	l.ID, _, _ = neo4j.GetRecordValue[string](record, "id")
+	l.Name, _, _ = neo4j.GetRecordValue[string](record, "name")
+	l.Email, _, _ = neo4j.GetRecordValue[string](record, "email")
+	l.Contact, _, _ = neo4j.GetRecordValue[string](record, "contact")
+	return l
+}
+
+func SearchLandlords(c *gin.Context) {
+	searchTerm, _ := c.GetQuery("q")
+	var query = "MATCH (n:Landlords) WHERE n.name =~ '(?i)^.*" + searchTerm + ".*$' RETURN n.id as id, n.name as name, n.email as email, n.contact as contact;"
+
+	res, err := database.ExecuteQuery(c, database.Query{String: query}, nil)
+	if err != nil {
+		return
+	}
+
+	landlords := make([]LandlordResult, len(res.Records))
+
+	for i, record := range res.Records {
+		landlords[i] = LandlordResult{Landlord{}.Reconstruct(record)}
+	}
+
+	c.IndentedJSON(http.StatusOK, landlords)
+}
+
+func GetLandlord(c *gin.Context) {
+	id := c.Param("id")
+	var query = "MATCH (n:Landlords) WHERE n.id = '" + id + "' RETURN n.id as id, n.name as name, n.email as email, n.contact as contact;"
+
+	res, err := database.ExecuteQuery(c, database.Query{String: query}, nil)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+	} else {
+		c.IndentedJSON(http.StatusOK, Landlord{}.Reconstruct(res.Records[0]))
+	}
+}
+
+func AddLandlord(c *gin.Context) {
+
+	l := new(Landlord)
+	err := c.Bind(l)
+
+	if err != nil {
+		return
+	}
+
+	var query = "CREATE (l:Landlords {id: randomUUID(), name: '" + l.Name + "', email: '" + l.Email + "', contact: '" + l.Contact + "'}) RETURN l;"
+
+	res, err := database.ExecuteQuery(c, database.Query{String: query, Type: "WRITE"}, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, res)
+}
